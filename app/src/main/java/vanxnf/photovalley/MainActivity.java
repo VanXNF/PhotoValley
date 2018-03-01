99d00b26c4dc9b74de70c82cdd753ca093ea5476
VanXNF <VanXNF@gmail.com>
2018-03-01 11:33:14 +0800

    加入背景图

--- app/src/main/java/vanxnf/photovalley/MainActivity.java
+++ app/src/main/java/vanxnf/photovalley/MainActivity.java
@@ -1,9 +1,13 @@
 package vanxnf.photovalley;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import vanxnf.photovalley.SnakeMenu.RelativeLayoutWithSnakeMenu;
@@ -15,6 +19,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.activity_main);
         //设置浮动按钮的点击事件
         snakeMenu = (RelativeLayoutWithSnakeMenu) findViewById(R.id.relative_layout);
