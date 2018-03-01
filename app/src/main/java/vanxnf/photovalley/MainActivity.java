131e73bef1f6a2c79145fc0a22d3c1111b73209f
VanXNF <VanXNF@gmail.com>
2018-03-01 10:59:55 +0800

    创建自定义控件 实现拖动蛇形悬浮按钮

--- app/src/main/java/vanxnf/photovalley/MainActivity.java
+++ app/src/main/java/vanxnf/photovalley/MainActivity.java
@@ -1,13 +1,28 @@
 package vanxnf.photovalley;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import vanxnf.photovalley.SnakeMenu.RelativeLayoutWithSnakeMenu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayoutWithSnakeMenu snakeMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //设置浮动按钮的点击事件
+        snakeMenu = (RelativeLayoutWithSnakeMenu) findViewById(R.id.relative_layout);
+        snakeMenu.setMenuListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "menu click", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
