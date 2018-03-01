3fe182887b7fca2b9f25efa68204b9b8f43db8bb
VanXNF <VanXNF@gmail.com>
2018-03-01 15:57:38 +0800

    更改图标配色

--- app/src/main/java/vanxnf/photovalley/MainActivity.java
+++ app/src/main/java/vanxnf/photovalley/MainActivity.java
@@ -1,14 +1,16 @@
 package vanxnf.photovalley;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Build;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 import vanxnf.photovalley.SnakeMenu.RelativeLayoutWithSnakeMenu;
 
@@ -16,9 +18,12 @@ public class MainActivity extends AppCompatActivity {
 
     private RelativeLayoutWithSnakeMenu snakeMenu;
 
+    private TextView LuckyText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //实现透明状态栏
         if (Build.VERSION.SDK_INT >= 21) {
             Window window = getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
@@ -26,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
         }
+
+        LuckyText = (TextView) findViewById(R.id.lucky_text);
 
         setContentView(R.layout.activity_main);
         //设置浮动按钮的点击事件
